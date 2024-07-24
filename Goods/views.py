015c06788f90afd3bbb321aafb0982880a8488c4
Your Name <your.email@example.com>
2024-07-24 16:44:33 +0500

    onlineshop

---
+++ Goods/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from . import models
+
+def main(request):
+    banners = models.Banner.objects.filter(is_active = True)[:5]
+    infos = models.Info.objects.last()
+    context = {
+        'banners': banners,
+        'info': infos
+    }
+
+    return render(request, 'index.html', context)
+
+
+def admin_page(request):
+    return render(request, 'admin')
+
+
+
+
